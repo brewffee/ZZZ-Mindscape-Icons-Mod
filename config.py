415c55a1d963a694374de3c34a30771e0769f6ca
bree ᓚᘏᗢ <68980781+brewffee@users.noreply.github.com>
2026-07-10 02:33:17 -0400

    Add Norma

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 MOD_NAME: str = "Mindscape Icons"
-MOD_VERSION: str = "3.0"
+MOD_VERSION: str = "3.1"
 
 # To update textures for a single agent, replace None with that agent's name
 CUR_AGENT: str | None = None
